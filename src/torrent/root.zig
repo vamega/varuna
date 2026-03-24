954abf21dab7a95be75fb66f2daaffc2c91d4790
Varun Madiath <github@madiathv.com>
2026-03-24 14:19:39 -0400

    torrent: add piece layout mapping

--- src/torrent/root.zig
+++ src/torrent/root.zig
@@ -1,3 +1,4 @@
 pub const bencode = @import("bencode.zig");
 pub const info_hash = @import("info_hash.zig");
 pub const metainfo = @import("metainfo.zig");
+pub const piece_layout = @import("piece_layout.zig");
